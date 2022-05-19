b6a148d55b2799fb81cf931012bb90ec6d04d49b
John Ruddock <john.jxr@pm.me>
2022-05-19 16:15:02 -0400

    Shopping cart persists in local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import { ProductContext } from './contexts/ProductContext';
@@ -9,9 +9,15 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+const cartFromLocalStorage = JSON.parse(localStorage.getItem('cart') || '[]')
+
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const [cart, setCart] = useState(cartFromLocalStorage)
+
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
 
 	const addItem = item => {
 		setCart([...cart, item])
